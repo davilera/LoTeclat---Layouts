a403316e4d377e27c9239a7ba8531424aca3b153
David Aguilera <david.aguilera@neliosoftware.com>
2018-01-03 14:42:27 +0100

    First upload.

---
+++ emojis.h
@@ -0,0 +1,115 @@
+#define UC4(a,b,c,d) I(1), D(LCTL), D(LSFT), T(U), U(LSFT), U(LCTL), T(a), T(b), T(c), T(d), T(ENT)
+#define UC5(a,b,c,d,e) I(1), D(LCTL), D(LSFT), T(U), U(LSFT), U(LCTL), T(a), T(b), T(c), T(d), T(e), T(ENT)
+
+/* 😇   */
+#define EC_ANGL M(1)
+#define EMOJI_ANGEL         UC5(1,F,6,0,7), END
+
+/* 💪   */
+#define EC_BCPS M(2)
+#define EMOJI_BICEPS        UC5(1,F,4,A,A), UC5(1,F,3,F,C), END
+
+/* 😎   */
+#define EC_SUNG M(3)
+#define EMOJI_SUNGLASSES    UC5(1,F,6,0,E), END
+
+/* 😐   */
+#define EC_DFCK M(4)
+#define EMOJI_DAFUCK        UC5(1,F,6,1,0), END
+
+/* 😡   */
+#define EC_RAGE M(5)
+#define EMOJI_RAGE          UC5(1,F,6,2,1), END
+
+/* 😳   */
+#define EC_FLSH M(6)
+#define EMOJI_FLUSHED       UC5(1,F,6,3,3), END
+
+/* 😁   */
+#define EC_GRIN M(7)
+#define EMOJI_GRIN          UC5(1,F,6,0,1), END
+
+/* 🤗   */
+#define EC_HUG  M(8)
+#define EMOJI_HUG           UC5(1,F,9,1,7), END
+
+/* 😍   */
+#define EC_INLV M(9)
+#define EMOJI_INLOVE        UC5(1,F,6,0,D), END
+
+/* ❤   */
+#define EC_HRT  M(10)
+#define EMOJI_HEART         UC4(2,7,6,4), END
+
+/* 😘   */
+#define EC_KISS M(11)
+#define EMOJI_KISS          UC5(1,F,6,1,8), END
+
+/* 😂   */
+#define EC_LOL  M(12)
+#define EMOJI_LOL           UC5(1,F,6,0,2), END
+
+/* 😱   */
+#define EC_MUN  M(13)
+#define EMOJI_MUNCH         UC5(1,F,6,3,1), END
+
+/* 😒   */
+#define EC_UNIM M(14)
+#define EMOJI_NOT_IMPRESSED UC5(1,F,6,1,2), END
+
+/* 😲   */
+#define EC_OMG  M(15)
+#define EMOJI_OMG           UC5(1,F,6,3,2), END
+
+/* 😤   */
+#define EC_PISS M(16)
+#define EMOJI_PISSED_OFF    UC5(1,F,6,2,4), END
+
+/* 😜   */
+#define EC_TWNK M(17)
+#define EMOJI_WINK_TONGUE   UC5(1,F,6,1,C), END
+
+/* 🤣   */
+#define EC_ROLL M(18)
+#define EMOJI_ROLL          UC5(1,F,9,2,3), END
+
+/* 😅   */
+#define EC_SWT  M(19)
+#define EMOJI_SWEAT_SMILE   UC5(1,F,6,0,5), END
+
+/* 🤔   */
+#define EC_THNK M(20)
+#define EMOJI_THINKING      UC5(1,F,9,1,4), END
+
+/* 👍   */
+#define EC_UP   M(21)
+#define EMOJI_THUMBS_UP     UC5(1,F,4,4,D), UC5(1,F,3,F,C), END
+
+/* ✌   */
+#define EC_VICT M(22)
+#define EMOJI_VICTORY       UC4(2,7,0,C), UC5(1,F,3,F,C), END
+
+/* 😉   */
+#define EC_RWNK M(23)
+#define EMOJI_WINK          UC5(1,F,6,0,9), END
+
+/* 😏   */
+#define EC_COOL M(24)
+#define EMOJI_COOL_SMILE    UC5(1,F,6,0,F), END
+
+/* 👌   */
+#define EC_OK  M(25)
+#define EMOJI_OK            UC5(1,F,4,4,C), UC5(1,F,3,F,C), END
+
+/* 😴   */
+#define EC_ZZZ  M(26)
+#define EMOJI_ZZZ           UC5(1,F,6,3,4), END
+
+/* 🤷   */
+#define EC_SHRG M(27)
+#define EMOJI_SHRUGGIE UC5(1,F,9,3,7), UC5(1,F,3,F,B), UC4(2,0,0,D), UC4(2,6,4,2), UC4(F,E,0,F), END
+
+/* 🤷   */
+#define EC_PALM M(28)
+#define EMOJI_PALM_FACE UC5(1,F,9,2,6), UC5(1,F,3,F,B), UC4(2,0,0,D), UC4(2,6,4,2), UC4(F,E,0,F), END
+
---
+++ keymaps/default/keymap.c
@@ -0,0 +1,184 @@
+#include "loteclat.h"
+#include "emojis.h"
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define ___T___ KC_TRNS
+#define XXXXXXX KC_NO
+
+// Layer shorthand
+#define _QW 0
+#define _LW 1
+#define _RS 2
+
+// Some custom constants
+#define KC_LEDS M(0)
+
+/* ROWS GRID */
+// .---------------------------------------------------------------------------------------------------------------.
+// |       |       |       |       |       |       |       |       |       |       |       |       |       |       |
+// |---------------------------------------------------------------------------------------------------------------|
+
+/* ROW 5 (CUSTOM) */
+// .---------------------------------------------------------------------------------------------------------------.
+// |       |         |         |         |                     |         |         |       |       |       |       |
+// '---------------------------------------------------------------------------------------------------------------'
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY - CUSTOM */
+// .---------------------------------------------------------------------------------------------------------------.
+// | `     | 1     | 2     | 3     | 4     | 5     | del   | 6     | 7     | 8     | 9     | 0     | -     | =     |
+// |---------------------------------------------------------------------------------------------------------------|
+// | tab   | q     | w     | e     | r     | t     | bcksp | y     | u     | i     | o     | p     | [     | ]     |
+// |---------------------------------------------------------------------------------------------------------------|
+// | lshif | a     | s     | d     | f     | g     | enter | h     | j     | k     | l     | ;     | '     | rshif |
+// |---------------------------------------------------------------------------------------------------------------|
+// | raise | z     | x     | c     | v     | b     | esc   | n     | m     | ,     | .     | /     | \     | caps  |
+// |---------------------------------------------------------------------------------------------------------------|
+// | lower | lgui    | lalt    | lctrl   | space               | rctrl   | ralt    | left  | down  | up    | right |
+// '---------------------------------------------------------------------------------------------------------------'
+
+ [_QW] = {
+  { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL  },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC },
+  { KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RSFT },
+  { MO(_RS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, KC_CAPS },
+  { MO(_LW), KC_LGUI, KC_LALT, XXXXXXX, KC_LCTL, XXXXXXX, KC_SPC,  XXXXXXX, KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
+ },
+
+/* QWERTY - LOWERED */
+// .---------------------------------------------------------------------------------------------------------------.
+// |       | F1    | F2    | F3    | F4    | F5    | insrt | F6    | F7    | F8    | F9    | F10   | F11   | F12   |
+// |---------------------------------------------------------------------------------------------------------------|
+// |       | power | wake  |       |       |       | volup | bltop |       |       |       | prscr |       |       |
+// |---------------------------------------------------------------------------------------------------------------|
+// |       |       | sleep |       |       |       | voldn | left  | down  | up    | right | leds  |       |       |
+// |---------------------------------------------------------------------------------------------------------------|
+// |       |       |       |       |       |       | mute  | blbot | blcam | blsid |       |       |       |       |
+// |---------------------------------------------------------------------------------------------------------------|
+// |       |         |         |         | play/pause          | prev    | next    | home  | pgdn  | pgup  | end   |
+// '---------------------------------------------------------------------------------------------------------------'
+
+ [_LW] = {
+  { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_INS,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12  },
+  { _______, KC_PWR,  KC_WAKE, _______, _______, _______, KC_VOLU, _______, KC_KP_7, _______, _______, KC_PSCR, _______, _______ },
+  { _______, _______, KC_SLEP, _______, _______, _______, KC_VOLD, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEDS, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, KC_MUTE, _______, KC_KP_1, KC_KP_5, KC_KP_3, _______, _______, _______ },
+  { _______, _______, KC_MPRV, XXXXXXX, KC_MNXT, XXXXXXX, KC_MPLY, XXXXXXX, _______, KC_KP_0, KC_HOME, KC_PGDN, KC_PGUP, KC_END  },
+ },
+
+/* QWERTY - RAISED */
+ [_RS] = {
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+  { _______, EC_TWNK, EC_RWNK, EC_RAGE, EC_ROLL, EC_THNK, _______, EC_OK,   EC_UP,   EC_INLV, EC_OMG,  EC_PISS, _______, _______ },
+  { _______, EC_ANGL, EC_SWT,  EC_DFCK, EC_FLSH, EC_GRIN, _______, EC_HUG,  EC_HRT,  EC_KISS, EC_LOL,  _______, _______, _______ },
+  { _______, EC_ZZZ,  EC_COOL, EC_SUNG, EC_VICT, EC_BCPS, _______, EC_UNIM, EC_MUN,  EC_SHRG, EC_PALM, _______, _______, _______ },
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+ },
+
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+
+};
+
+const macro_t *action_get_macro( keyrecord_t *record, uint8_t id, uint8_t opt ) {
+
+	if ( ! record->event.pressed ) {
+		return MACRO_NONE;
+	}
+
+	if ( 0 == id ) {
+		#ifdef BACKLIGHT_ENABLE
+			backlight_step();
+		#endif
+		return MACRO_NONE;
+	}//end if
+
+
+	switch( id ) {
+
+		case 1:  return MACRO( EMOJI_ANGEL );
+		case 2:  return MACRO( EMOJI_BICEPS );
+		case 3:  return MACRO( EMOJI_SUNGLASSES );
+		case 4:  return MACRO( EMOJI_DAFUCK );
+		case 5:  return MACRO( EMOJI_RAGE );
+		case 6:  return MACRO( EMOJI_FLUSHED );
+		case 7:  return MACRO( EMOJI_GRIN );
+		case 8:  return MACRO( EMOJI_HUG );
+		case 9:  return MACRO( EMOJI_INLOVE );
+		case 10: return MACRO( EMOJI_HEART );
+		case 11: return MACRO( EMOJI_KISS );
+		case 12: return MACRO( EMOJI_LOL );
+		case 13: return MACRO( EMOJI_MUNCH );
+		case 14: return MACRO( EMOJI_NOT_IMPRESSED );
+		case 15: return MACRO( EMOJI_OMG );
+		case 16: return MACRO( EMOJI_PISSED_OFF );
+		case 17: return MACRO( EMOJI_WINK_TONGUE );
+		case 18: return MACRO( EMOJI_ROLL );
+		case 19: return MACRO( EMOJI_SWEAT_SMILE );
+		case 20: return MACRO( EMOJI_THINKING );
+		case 21: return MACRO( EMOJI_THUMBS_UP );
+		case 22: return MACRO( EMOJI_VICTORY );
+		case 23: return MACRO( EMOJI_WINK );
+		case 24: return MACRO( EMOJI_COOL_SMILE );
+		case 25: return MACRO( EMOJI_OK );
+		case 26: return MACRO( EMOJI_ZZZ );
+		case 27: return MACRO( EMOJI_SHRUGGIE );
+		case 28: return MACRO( EMOJI_PALM_FACE );
+
+	}
+
+	return MACRO_NONE;
+};
+
+
+/* Turn LEDS off automatically after a while... */
+
+#define LEDS_ON  0
+#define LEDS_DIM 1
+#define LEDS_OFF 2
+
+#define LED_DIM_TIME 30000
+#define LED_OFF_TIME 300000
+
+int leds_mode = LEDS_ON;
+int led_intensity;
+int last_press;
+
+bool process_record_user( uint16_t keycode, keyrecord_t *record ) {
+
+	last_press = timer_read();
+	if ( LEDS_ON != leds_mode ) {
+		leds_mode = LEDS_ON;
+		backlight_set( led_intensity );
+	}//end if
+
+	return true;
+
+};
+
+void matrix_scan_user( void ) {
+
+	int time = timer_elapsed( last_press );
+
+	if ( ! get_backlight_level() ) {
+		return;
+	}//end if
+
+	if ( LED_OFF_TIME < time ) {
+		leds_mode = LEDS_OFF;
+		backlight_set( 0 );
+		return;
+	}//end if
+
+	if ( LED_DIM_TIME < time ) {
+		leds_mode = LEDS_DIM;
+		backlight_set( 1 );
+		return;
+	}//end if
+
+	led_intensity = get_backlight_level();
+
+};
